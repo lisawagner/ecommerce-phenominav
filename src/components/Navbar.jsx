99798bc10a196f47050eae7c2c168b7df7617c92
Lisa Wagner <lisawagner.online@gmail.com>
2022-04-29 16:10:17 -0700

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <h1>Navbar</h1>
+      {/* TOP-NAV */}
+      <div className="top-nav">
+        <div className="container">
+          <div className="top-nav__row">
+            <div className="search-col">
+
+            </div>
+            <div className="brand-col">
+
+            </div>
+            <div className="icon-links-col">
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+      {/* MAIN-NAV */}
+    
+    </>
+
+  )
+}
+
+export default Navbar
