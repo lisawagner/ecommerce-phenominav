487d52e4d9a3df39a0785613da566f75a18c74b2
Lisa Wagner <lisawagner.online@gmail.com>
2022-05-01 07:06:55 -0700

    added responsive logic

--- src/components/Navbar2/Navbar2.jsx
+++ src/components/Navbar2/Navbar2.jsx
@@ -26,9 +26,24 @@ const Navbar2 = () => {
         <li className="item"><Link to='/'>actiwear</Link></li>
         <li className="item"><Link to='/'>accessories</Link></li>
         <li className="item"><Link to='/'>contact</Link></li>
-        <li className="item button"><Link to='/login'>login</Link></li>
-        <li className="item button secondary"><Link to='/favourites'>favourites</Link></li>
-        <li className="item button tertiary"><Link to='/cart'>cart</Link></li>
+        <li className="item button">
+          <Link to='/login'>
+            <MdPersonOutline />
+            <span className="btn-label" >login</span>
+          </Link>
+        </li>
+        <li className="item button secondary">
+          <Link to='/favourites'>
+            <MdOutlineFavoriteBorder />
+            <span className="btn-label">favourites</span>
+          </Link>
+        </li>
+        <li className="item button tertiary">
+          <Link to='/cart'>
+            <MdOutlineShoppingCart />
+            <span className="btn-label">cart</span>
+          </Link>
+        </li>
         <li className="menu-toggle" onClick={handleToggle}>
           {!toggle ? (
             <MdOutlineMenu />
