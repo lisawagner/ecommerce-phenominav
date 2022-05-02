de1cc53ab0b53c8cf78aa6e7f8f4073da6c1ea1e
Lisa Wagner <lisawagner.online@gmail.com>
2022-05-02 07:42:02 -0700

    navbar 3 scaffolding

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 // import Navbar from '../Navbar/Navbar'
 import Footer from '../Footer/Footer'
-import Navbar2 from '../Navbar2/Navbar2'
+import Navbar3 from '../Navbar3/Navbar3'
+// import Navbar2 from '../Navbar2/Navbar2'
 
 const Layout = ({children}) => {
   return (
     <>
       {/* <Navbar /> */}
-      <Navbar2 />
+      {/* <Navbar2 /> */}
+      <Navbar3 />
       {children}
       <Footer />
     </>
---
+++ src/components/Navbar3/Navbar3.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import {
+  MdSearch,
+  MdPersonOutline,
+  MdOutlineFavoriteBorder,
+  MdOutlineShoppingCart,
+  MdOutlineMenu,
+  MdClose
+  } from 'react-icons/md'
+
+import './navbar3.css'
+
+const Navbar3 = () => {
+  return (
+    <div>Navbar3</div>
+  )
+}
+
+export default Navbar3
